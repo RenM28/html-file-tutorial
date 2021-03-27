afc2d59df1bab5dbe57d3f269f5919625168b3b1
RenM28 <70777074+RenM28@users.noreply.github.com>
2021-03-26 19:39:35 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require('http')
+const fs = require('fs')
+const port = process.env.PORT || 3000
+
+// reads files
+function serveStaticFile(res, path, contentType, responseCode = 200) {
+	// asynchronous method for reading files
+	fs.readFile(__dirname + path, (err, data) => {
+		if(err) { // server error
+			res.writeHead(500, { 'Content-Type': 'text/plain' })
+			return res.end('500 - Internal Error') 
+		}
+		// file sent to client w/ response code
+		res.writeHead(responseCode, { 'Content-Type': contentType })
+		res.end(data)
+	})
+}
+
+const server = http.createServer((req,res) => {
+	// normalize url
+	const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
+	switch(path) {
+		case '':
+			serveStaticFile(res, '/public/home.html', 'text/html')
+			break
+		case '/about':
+			serveStaticFile(res, '/public/about.html', 'text/html')
+			break
+		case '/img/logo.jpg':
+			serveStaticFile(res, '/public/img/logo.jpg', 'image/jpg')
+			break
+		default:
+			serveStaticFile(res, '/public/404.html', 'text/html', 404)
+			break
+	}
+})
+
+server.listen(port, () => console.log(`server started on port ${port}; ` +
+'press Ctrl-C to terminate....'))
+			
